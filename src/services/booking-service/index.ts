7e17bf8c4d1696eac0d88cf12020ce656651970e
Caio Ruiz Coldebella <63621516+Caio-Coldebella@users.noreply.github.com>
2022-12-20 13:34:54 -0300

    feat: adicao da rota getBookingByRoomId (#2)

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -37,6 +37,15 @@ async function getBooking(userId: number) {
   return booking;
 }
 
+async function getBookingByRoomId(roomId: number) {
+  const booking = await bookingRepository.findByRoomId(roomId);
+  if(!booking) {
+    throw notFoundError();
+  }
+
+  return booking;
+}
+
 async function bookingRoomById(userId: number, roomId: number) {
   await checkEnrollmentTicket(userId);
   await checkValidBooking(roomId);
@@ -62,6 +71,7 @@ async function changeBookingRoomById(userId: number, roomId: number) {
 const bookingService = {
   bookingRoomById,
   getBooking,
+  getBookingByRoomId,
   changeBookingRoomById,
 };
 
