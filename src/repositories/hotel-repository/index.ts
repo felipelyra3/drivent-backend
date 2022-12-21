4df4f483540642c7f25ced4a9173398ad885e198
Ana Beatriz Aranha Rodrigues <anab.aranha@gmail.com>
2022-12-21 15:38:57 -0300

    Feat/hotels (#5)
    * feat: get vacacies from each hotel by HotelId
    
    * fix: prettier

--- src/repositories/hotel-repository/index.ts
+++ src/repositories/hotel-repository/index.ts
@@ -11,13 +11,21 @@ async function findRoomsByHotelId(hotelId: number) {
     },
     include: {
       Rooms: true,
-    }
+    },
   });
+}
+
+async function findHotelVacancies(
+  id: number,
+): Promise<{ hotelId: number; totalBookings: number; totalCapacity: number; vacancies: number }[]> {
+  return prisma.$queryRaw`SELECT "Hotel".id As "hotelId", Sum(b.capacity) AS "totalCapacity", SUM(b.ocupados) AS "totalBookings", Sum(b.capacity) - SUM(b.ocupados) AS vacancies FROM "Hotel" LEFT JOIN (SELECT "Room".capacity, "Room"."hotelId", Count("Booking"."roomId") AS ocupados FROM "Room" LEFT JOIN "Booking" ON "Booking"."roomId" = "Room".id GROUP BY "Booking"."roomId", "Room".id) AS b ON "Hotel".id = b."hotelId" WHERE "Hotel".id =${id} GROUP BY "Hotel".id;
+`;
 }
 
 const hotelRepository = {
   findHotels,
   findRoomsByHotelId,
+  findHotelVacancies,
 };
 
 export default hotelRepository;
