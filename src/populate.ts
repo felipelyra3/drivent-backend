10b12683ee3853480ad7ec9cea0660b99733899a
ana-aranha <anab.aranha@gmail.com>
2022-12-22 17:01:00 -0300

    feat: script create new ticket types

--- src/populate.ts
+++ src/populate.ts
@@ -11,9 +11,10 @@ import {
   createPayment,
   generateCreditCardData,
   createTicketTypeWithHotel,
-  createTicketTypeRemote,
   createBooking,
-  createHotel
+  createTicketTypeWithoutHotel,
+  createTicketTypeRemote,
+  createHotel,
 } from "../tests/factories";
 
 async function createRoom(hotelId: number, roomname: string) {
@@ -22,13 +23,15 @@ async function createRoom(hotelId: number, roomname: string) {
       name: roomname,
       capacity: Math.floor(Math.random() * (3 - 1 + 1) + 1), //entre 1 e 3
       hotelId: hotelId,
-    }
+    },
   });
 }
 
 async function createdata() {
   const ticketType = await createTicketTypeWithHotel();
-  const ticket = await createTicket(1, ticketType.id, TicketStatus.PAID);// parametro 1 representa o id do enrollment
+  await createTicketTypeWithoutHotel();
+  await createTicketTypeRemote();
+  const ticket = await createTicket(1, ticketType.id, TicketStatus.PAID); // parametro 1 representa o id do enrollment
   const payment = await createPayment(ticket.id, ticketType.price);
   const createdHotel1 = await createHotel();
   createRooms(createdHotel1.id);
@@ -36,16 +39,16 @@ async function createdata() {
   createRooms(createdHotel2.id);
   const createdHotel3 = await createHotel();
   createRooms(createdHotel3.id);
-} 
+}
 
 async function createRooms(hotelid: number) {
-  for(let i=0; i<50; i++) {
-    const room = await createRoom(hotelid, `${i+1}`);
-    if(i%2===0) {
+  for (let i = 0; i < 50; i++) {
+    const room = await createRoom(hotelid, `${i + 1}`);
+    if (i % 2 === 0) {
       const newuser = await createUser();
       await createBooking({
         userId: newuser.id,
-        roomId: room.id
+        roomId: room.id,
       });
     }
   }
@@ -56,4 +59,3 @@ const port = +process.env.PORT || 4000;
 init().then(() => {
   createdata();
 });
-
