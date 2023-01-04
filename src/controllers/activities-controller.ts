8c9a615d12ca8901d447f874284c77186bbc8b55
Ana Beatriz Aranha Rodrigues <anab.aranha@gmail.com>
2023-01-04 14:29:06 -0300

    feat: activity subscribe logic without checking free time (#13)

--- src/controllers/activities-controller.ts
+++ src/controllers/activities-controller.ts
@@ -9,5 +9,26 @@ export async function getActivities(req: AuthenticatedRequest, res: Response) {
     return res.status(httpStatus.OK).send(activities);
   } catch (error) {
     return res.sendStatus(httpStatus.BAD_REQUEST);
-  }         
+  }
+}
+
+export async function activitySubscription(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { userId } = req;
+
+    const { activityId } = req.body;
+
+    if (!activityId) {
+      return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+
+    const subscription = await activitiesService.createSubscription(userId, Number(activityId));
+
+    return res.status(httpStatus.OK).send({ subscriptionId: subscription.id });
+  } catch (error) {
+    if (error.name === "CannotSubscribeError") {
+      return res.sendStatus(httpStatus.FORBIDDEN);
+    }
+    return res.sendStatus(httpStatus.NOT_FOUND);
+  }
 }
