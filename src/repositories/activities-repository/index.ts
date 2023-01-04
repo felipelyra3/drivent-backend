694ae692268f4e48f2c4e535d6e0678124d33d80
ana-aranha <anab.aranha@gmail.com>
2023-01-04 13:40:05 -0300

    feat: activity subscribe logic without checking free time

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,11 +1,33 @@
 import { prisma } from "@/config";
+import { ActivitySubscription } from "@prisma/client";
+
+type CreateParams = Omit<ActivitySubscription, "id">;
 
 async function findActivities() {
   return prisma.activities.findMany();
 }
 
+async function findByActivityId(activityId: number) {
+  return prisma.activities.findFirst({
+    where: {
+      id: activityId,
+    },
+  });
+}
+
+async function create({ activityId, ticketId }: CreateParams): Promise<ActivitySubscription> {
+  return prisma.activitySubscription.create({
+    data: {
+      activityId,
+      ticketId,
+    },
+  });
+}
+
 const activitiesRepository = {
   findActivities,
+  create,
+  findByActivityId,
 };
-  
+
 export default activitiesRepository;
