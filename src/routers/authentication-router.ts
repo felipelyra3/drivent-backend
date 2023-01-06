ca1e188d8996c150e61274bdd7c36c4ae70ffa49
felipelyra3 <felipelyra3@gmail.com>
2023-01-06 18:26:49 -0300

    feat: login with github

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -2,9 +2,12 @@ import { singInPost } from "@/controllers";
 import { validateBody } from "@/middlewares";
 import { signInSchema } from "@/schemas";
 import { Router } from "express";
+import { singInGitHubPost } from "@/controllers/authentication-github-controller";
 
 const authenticationRouter = Router();
 
-authenticationRouter.post("/sign-in", validateBody(signInSchema), singInPost);
+authenticationRouter
+  .post("/sign-in/github", singInGitHubPost)
+  .post("/sign-in", validateBody(signInSchema), singInPost);
 
 export { authenticationRouter };
