76343168a877702ae9ad624202ec1a5979907f32
ana-aranha <anab.aranha@gmail.com>
2023-01-11 20:58:56 -0300

    refactor: using transactions in the enrollment and address update function

--- src/repositories/enrollment-repository/index.ts
+++ src/repositories/enrollment-repository/index.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/config";
-import { Enrollment } from "@prisma/client";
+import { Enrollment, Address } from "@prisma/client";
 
 async function findWithAddressByUserId(userId: number) {
   return prisma.enrollment.findFirst({
@@ -12,7 +12,7 @@ async function findWithAddressByUserId(userId: number) {
 
 async function findById(enrollmentId: number) {
   return prisma.enrollment.findFirst({
-    where: { id: enrollmentId }
+    where: { id: enrollmentId },
   });
 }
 
@@ -20,18 +20,36 @@ async function upsert(
   userId: number,
   createdEnrollment: CreateEnrollmentParams,
   updatedEnrollment: UpdateEnrollmentParams,
+  createdAddress: CreateAddressParams,
+  updatedAddress: UpdateAddressParams,
 ) {
-  return prisma.enrollment.upsert({
+  const enrollment = await prisma.enrollment.upsert({
     where: {
       userId,
     },
     create: createdEnrollment,
     update: updatedEnrollment,
   });
+
+  const address = prisma.address.upsert({
+    where: {
+      enrollmentId: enrollment.id,
+    },
+    create: {
+      ...createdAddress,
+      Enrollment: { connect: { id: enrollment.id } },
+    },
+    update: updatedAddress,
+  });
+
+  return prisma.$transaction([address]);
 }
 
 export type CreateEnrollmentParams = Omit<Enrollment, "id" | "createdAt" | "updatedAt">;
 export type UpdateEnrollmentParams = Omit<CreateEnrollmentParams, "userId">;
+
+export type CreateAddressParams = Omit<Address, "id" | "createdAt" | "updatedAt" | "enrollmentId">;
+export type UpdateAddressParams = CreateAddressParams;
 
 const enrollmentRepository = {
   findWithAddressByUserId,
